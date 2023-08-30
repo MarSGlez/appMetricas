0a16bc2850646dcb23373af4abd9161720e19023
MarSGlez <martin.sanchezg@hotmail.com>
2023-08-29 19:48:00 -0600

    cambios en documentos ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Metricas', url: '/folder/documentos', icon: 'mail' },
+    { title: 'Metricas', url: '/folder/metricas', icon: 'mail' },
+    { title: 'Documentos', url: '/vista/documentos', icon: 'warning' },
     { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
     { title: 'Archived', url: '/folder/archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/spam', icon: 'warning' },
-    { title: 'Documentos', url: '/vista/documentos', icon: 'warning' },
   ];
   //public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
