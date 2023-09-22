c03df51456994d782012ef23942d1d21ac4c5b14
MarSGlez <martin.sanchezg@hotmail.com>
2023-09-21 18:36:52 -0600

    cambios menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public appPages = [
     { title: 'Metricas', url: '/folder/metricas', icon: 'mail' },
-    { title: 'Documentos', url: '/vista/documentos', icon: 'warning' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Nuevo Documento', url: '/vista/documentos', icon: 'warning' },
+    //{ title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
+    //{ title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
+    //{ title: 'Archived', url: '/folder/archived', icon: 'archive' },
+    //{ title: 'Trash', url: '/folder/trash', icon: 'trash' },
+    //{ title: 'Spam', url: '/folder/spam', icon: 'warning' },
   ];
   //public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
